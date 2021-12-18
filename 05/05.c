d00b040880de6cfdc9fd4462f1c9c837de998d03
Samuel Rembisz <sjrembisz07@gmail.com>
2021-12-18 02:27:44 +0000

    Started day 5

---
+++ 05/05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#if 1
+#define FILE_NAME "sample.txt"
+#else
+#define FILE_NAME "input.txt"
+#endif
+
+int main(void) {
+
+    
+
+    return EXIT_SUCCESS;
+}
